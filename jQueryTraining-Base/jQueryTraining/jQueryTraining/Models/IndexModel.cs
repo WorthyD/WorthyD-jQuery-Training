5a56f16f0094ae07ad762ad17ccf73472318a680
Daniel Worthy <daniel.worthy@rockfishmail.com>
2013-11-05 20:39:23 -0600

    Initial Commit

---
+++ jQueryTraining-Base/jQueryTraining/jQueryTraining/Models/IndexModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jQueryTraining.Models
+{
+    public class IndexModel
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string emailAddress { get; set; }
+        public string slType { get; set; }
+        public string acceptTerms { get; set; }
+    }
+}
---
+++ jQueryTraining-Master/jQueryTraining/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace jQueryTraining.Controllers {
+    public class HomeController : Controller {
+        public ActionResult Index() {
+            Models.IndexModel model = new Models.IndexModel();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult Index(Models.IndexModel postModel) {
+
+            return View(postModel);
+        }
+
+        public ActionResult About() {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact() {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
