785aebf9fefbf36292b98f82bc62bce32a210cec
Daniel Worthy <daniel.worthy@rockfishmail.com>
2013-11-06 12:49:50 -0600

    Working on stage one

--- jQueryTraining-Base/jQueryTraining/jQueryTraining/Global.asax.cs
+++ jQueryTraining-Base/jQueryTraining/jQueryTraining/Global.asax.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Optimization;
+
 using System.Web.Routing;
 
 namespace jQueryTraining
@@ -15,7 +15,7 @@ namespace jQueryTraining
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+   
         }
     }
 }
